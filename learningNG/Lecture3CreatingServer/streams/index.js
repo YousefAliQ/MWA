a065b29b6d7cdf3cc18d9eae49685c27a05de426
yousefaliq <yquran@mum.edu>
2020-02-26 21:26:32 -0600

    reading and writing files using streams

---
+++ learningNG/Lecture3CreatingServer/streams/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+// this example solved the memory issue using streams. But if the destination is slow
+// a bottleneck problem will appear. And it will solved by pipe 
+const readable = fs.createReadStream(path.join(__dirname,'../files/greet.txt'),
+                                                {encoding:'utf-8',highWaterMark: 16 * 1024});
+const writable = fs.createWriteStream(path.join(__dirname,'./greet.txt'));
+
+readable.on('data',function(chunk){
+    console.log(chunk);
+    writable.write(chunk);
+});
